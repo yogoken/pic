d078186ca1637d9eadc44e5d25c0832baf08727b
yogoken <yogo.k@husky.neu.edu>
2016-11-05 16:10:30 +0900

    Add scraping letter model

--- app/models/letter.rb
+++ app/models/letter.rb
@@ -1,2 +1,19 @@
 class Letter < ApplicationRecord
+  validates :url, format: URI::regexp(%w(http https))
+
+  def create_letter
+    agent = Mechanize.new
+    page = agent.get(self.url)
+    elements = page.search('meta')
+    letter = {}
+    elements.each do |ele|
+      property = ele.get_attribute(:property)
+      if property == "og:title"
+        letter[:title] = ele.get_attribute(:content)
+      elsif property == "og:image"
+        letter[:image] = ele.get_attribute(:content)
+      end
+    end
+    self.update(letter)
+  end
 end
