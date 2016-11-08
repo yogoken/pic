f66b4100f65eb7505d04a33442947d7458f5ad0a
yogoken <yogo.k@husky.neu.edu>
2016-11-08 22:28:27 +0900

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'letters#index'
-  resources :letters, only: [:create, :show]
+  resources :letters, only: [:create, :show] do
+    resources :comments, only: [:create, :destroy] do
+      resources :likes, only: [:create, :destroy]
+    end
+  end
   resources :users, only: [:show, :edit, :update]
+  devise_for :users
 end
