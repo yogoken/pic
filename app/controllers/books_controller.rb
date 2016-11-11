fd461a9b75240199950c6e064eea1b9c488e78a6
yogoken <yogo.k@husky.neu.edu>
2016-11-11 22:45:09 +0900

    Add books controller

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,40 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user = User.find(params[:user_id])
+    @books = Book.where(user_id: @user)
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @search_books = Book.search_book(params[:keyword])
+  end
+
+  def create
+    book = Book.new(create_params)
+    if book.save
+      flash[:notice] = ""
+      redirect_to user_book_path(current_user, book)
+    else
+      flash[:alert] = "おすすめ本の投稿に失敗しました。"
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:book).permit(
+      :title,
+      :image,
+      :url,
+      :author,
+      :manufacturer)
+      .merge(user_id: current_user.id
+    )
+  end
+end
