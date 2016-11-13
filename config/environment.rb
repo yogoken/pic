beb8a7cf0afdde87efa8c492893b8918f7df2ea6
yogoken <yogo.k@husky.neu.edu>
2016-11-13 18:48:34 +0900

    Tried to add amazon api

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Amazon::Ecs.options = {
+  associate_tag: ENV['AMAZON_ASSOCIATE_ID'],
+  AWS_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  AWS_secret_access_key_id: ENV['AWS_SECRET_ACCESS_KEY_ID']
+}
