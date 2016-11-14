081049c914b3f36d5729b57002cb390748e75971
yogoken <yogo.k@husky.neu.edu>
2016-11-14 17:29:02 +0900

    Update environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,8 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-Amazon::Ecs.options = {
-  associate_tag: ENV['AMAZON_ASSOCIATE_ID'],
-  AWS_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  AWS_secret_access_key_id: ENV['AWS_SECRET_ACCESS_KEY_ID']
-}
+Amazon::Ecs.configure do |options|
+  options[:AWS_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
+  options[:AWS_secret_key] = ENV['AWS_SECRET_ACCESS_KEY_ID']
+  options[:associate_tag] = ENV['AMAZON_ASSOCIATE_ID']
+end
