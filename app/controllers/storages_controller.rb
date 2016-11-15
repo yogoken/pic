a6249ab127db9d261dc876e43a7c373ddc329428
yogoken <yogo.k@husky.neu.edu>
2016-11-15 19:43:48 +0900

    Add storages  controller

---
+++ app/controllers/storages_controller.rb
@@ -0,0 +1,30 @@
+class StoragesController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    storage = Storage.new(create_params)
+    if storage.save
+      flash[:notice] = ''
+      redirect_to :back
+    else
+      flash[:alert] = 'ニュース保管リストに登録できませんでした。'
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    if Storage.find(params[:id]).destroy
+      flash[:notice] = 'ニュース保管リストから取り除きました。'
+      redirect_to :back
+    else
+      flash[:alert] = 'ニュース保管リストから取り除けませんでした。'
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def create_params
+    params.require(:storage).permit(:letter_id).merge(user_id: current_user.id)
+  end
+end
