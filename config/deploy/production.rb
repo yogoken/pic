9f87ce5e9cec66f0381b5288f4defb773769d0cf
yogoken <yogo.k@husky.neu.edu>
2016-11-15 15:46:36 +0900

    Delete unnecessary codes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,9 +2,3 @@
 server '52.199.29.59', user: 'yogoken', roles: %w{ app db web }
 # デプロイするサーバーにsshログインする鍵の情報を記述
 set :ssh_options, keys: '~/.ssh/keypair_rsa'
-
-Amazon::Ecs.configure do |options|
-  options[:AWS_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
-  options[:AWS_secret_key] = ENV['AWS_SECRET_ACCESS_KEY_ID']
-  options[:associate_tag] = ENV['AMAZON_ASSOCIATE_ID']
-end
