061eb81f1262bd967fa870f8b47a323260739da0
yogoken <yogo.k@husky.neu.edu>
2016-11-15 20:02:33 +0900

    Comment out production error file

--- db/migrate/20161112090408_remove_group_id_to_users.rb
+++ db/migrate/20161112090408_remove_group_id_to_users.rb
@@ -1,5 +1,5 @@
-class RemoveGroupIdToUsers < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :users, :group_id, :integer
-  end
-end
+#class RemoveGroupIdToUsers < ActiveRecord::Migration[5.0]
+ # def change
+  #  remove_column :users, :group_id, :integer
+  #end
+#end
