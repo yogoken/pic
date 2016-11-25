5635f444bd141ae95cd1802f9e760fb61e4a2f9c
yogoken <yogo.k@husky.neu.edu>
2016-11-25 21:28:53 +0900

    Will add create place in view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,22 @@
 class MessagesController < ApplicationController
   def index
+    @message  = Message.new
     @messages = Message.all
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @messages = Message.all
+    if @message.save
+      redirect_to messages_path, notice: 'return message successfully'
+      flash[:alert] = @message.errors.full_messages.join(',')
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161121134752) do
+ActiveRecord::Schema.define(version: 20161125100417) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -62,6 +62,12 @@ ActiveRecord::Schema.define(version: 20161121134752) do
     t.datetime "updated_at", null: false
     t.index ["comment_id"], name: "index_likes_on_comment_id", using: :btree
     t.index ["user_id"], name: "index_likes_on_user_id", using: :btree
+  end
+
+  create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.text     "body",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
   create_table "storages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
