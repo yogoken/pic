337af6f743c325e542b4cb6daa8a8709202b4596
yogoken <yogo.k@husky.neu.edu>
2016-11-29 20:00:26 +0900

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125100417) do
+ActiveRecord::Schema.define(version: 20161129093721) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -22,6 +22,21 @@ ActiveRecord::Schema.define(version: 20161125100417) do
     t.datetime "updated_at",                              null: false
     t.text     "url",          limit: 65535
     t.index ["user_id"], name: "index_books_on_user_id", using: :btree
+  end
+
+  create_table "chat_group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "chat_group_id", null: false
+    t.integer  "user_id",       null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.index ["chat_group_id"], name: "index_chat_group_users_on_chat_group_id", using: :btree
+    t.index ["user_id"], name: "index_chat_group_users_on_user_id", using: :btree
+  end
+
+  create_table "chat_groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -65,9 +80,11 @@ ActiveRecord::Schema.define(version: 20161125100417) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "body",       limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.text     "body",          limit: 65535
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "user_id",                     null: false
+    t.integer  "chat_group_id",               null: false
   end
 
   create_table "storages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -94,6 +111,7 @@ ActiveRecord::Schema.define(version: 20161125100417) do
     t.datetime "updated_at",                          null: false
     t.string   "nickname"
     t.string   "avatar"
+    t.integer  "admin",                  default: 0
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
