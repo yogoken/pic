d9092d8020617493ee11586dd41f4bb1030462b6
yogoken <yogo.k@husky.neu.edu>
2016-12-14 22:00:01 +0900

    Add new/create/destroy action on admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,38 @@
 class Admin::UsersController < AdminController
+
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+    else
+      redirect_to new_admin_user_path
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :id,
+      :email,
+      :nickname,
+      :avatar,
+      :password,
+      :admin
+    )
+  end
+
 end
