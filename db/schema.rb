d9da9e5596e3ca91505e67edd1669ad2aa3321f1
yogoken <yogo.k@husky.neu.edu>
2016-12-15 13:58:42 +0900

    Create admin migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161129093721) do
+ActiveRecord::Schema.define(version: 20161215045729) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
