e50f055e7408909d9299c0a92b1d9cba850876b1
yogoken <yogo.k@husky.neu.edu>
2016-12-17 12:28:22 +0900

    Update users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :user do
     email     { Faker::Internet.email }
     password  { Faker::Internet.password(8) }
-    nickname  { Faker::Internet.user_name }
+    nickname  'hoge'
     avatar    'test.png'
 
     factory :owner do
