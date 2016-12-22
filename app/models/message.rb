b19ef0f20084729a6df88133eb83bfba8c3d326e
yogoken <yogo.k@husky.neu.edu>
2016-12-22 21:32:19 +0900

    Delete unneccesary controllers/models

--- app/models/message.rb
+++
@@ -1,8 +0,0 @@
-class Message < ApplicationRecord
-  belongs_to :user
-  belongs_to :chat_group
-
-  validates :user, presence: true
-  validates :chat_group, presence: true
-  validates :body, presence: true
-end
