d2e6f670c852135b730da038fecefc234745cd05
yogoken <yogo.k@husky.neu.edu>
2017-02-20 17:29:31 +0900

    Fix 'Style/SpaceInsidePercentLiteralDelimiters'

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 # EC2サーバーのIP, EC2サーバーにログインするユーザー名, サーバーのロールを記述
-server '52.199.29.59', user: 'yogoken', roles: %w( app db web )
+server '52.199.29.59', user: 'yogoken', roles: %w(app db web)
 # デプロイするサーバーにsshログインする鍵の情報を記述
 set :ssh_options, keys: '~/.ssh/keypair_rsa'
